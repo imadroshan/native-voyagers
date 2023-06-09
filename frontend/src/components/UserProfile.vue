55858c2a97189ab8e63b8dfb74236f38548da06a
Imad Roshan <imad.roshan@gmail.com>
2023-06-09 16:05:49 +0200

    Includes the location field in user profile
    - and updates it each time the marker is dragged

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -7,6 +7,7 @@
           <v-text-field label="Email" v-model="email" required></v-text-field>
           <v-text-field label="Password" v-model="password" type="password" required></v-text-field>
           <v-text-field label="Avatar URL" v-model="avatarUrl"></v-text-field>
+          <v-text-field label="Location" v-model="locationString" readonly></v-text-field>
           <v-btn color="green darken-1" text type="submit">Update</v-btn>
         </v-form>
       </v-col>
@@ -16,7 +17,7 @@
     </v-row>
   </v-container>
 </template>
-  
+
 <script>
 import axios from 'axios'
 import mapboxgl from 'mapbox-gl'
@@ -34,6 +35,11 @@ export default {
       marker: null
     }
   },
+  computed: {
+    locationString() {
+      return `Longitude: ${this.location[0]}, Latitude: ${this.location[1]}`;
+    }
+  },
   mounted() {
     // Initialize map
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN
@@ -41,7 +47,7 @@ export default {
       container: this.$refs.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: this.location,
-      zoom: 2
+      zoom: 5
     })
 
     // Add geocoder control
@@ -54,7 +60,12 @@ export default {
     // Create draggable marker
     this.marker = new mapboxgl.Marker({
       draggable: true
-    }).setLngLat(this.location).addTo(this.map)
+    })
+    .setLngLat(this.location)
+    .addTo(this.map)
+    .on('dragend', () => {
+      this.location = this.marker.getLngLat().toArray();
+    });
 
     // Set marker position to geocoder result
     geocoder.on('result', e => {
@@ -64,13 +75,15 @@ export default {
       
       this.marker = new mapboxgl.Marker({ draggable: true })
         .setLngLat(e.result.geometry.coordinates)
-        .addTo(this.map);
-    })
-},
+        .addTo(this.map)
+        .on('dragend', () => {
+          this.location = this.marker.getLngLat().toArray();
+        });
+    });
+  },
   methods: {
     handleFormSubmit() {
-      const lngLat = this.marker.getLngLat();
-      this.submitForm(lngLat);
+      this.submitForm(this.location);
     },
     async submitForm(lngLat) {
       try {
