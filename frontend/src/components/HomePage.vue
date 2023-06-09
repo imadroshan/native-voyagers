d534c229be7dc30128770991d3f6931e03a59de5
Imad Roshan <imad.roshan@gmail.com>
2023-06-10 01:33:39 +0200

    Fixes the blog carousel

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -5,11 +5,9 @@
 <script>
 import axios from 'axios'
 import mapboxgl from 'mapbox-gl'
-import Vue from 'vue'
-import { Carousel } from 'vuetify'
-import Vuetify from 'vuetify'
+import { Swiper as SwiperClass, Navigation, Pagination } from 'swiper'
 
-Vue.use(Vuetify)
+SwiperClass.use([Navigation, Pagination]);
 
 export default {
   data() {
@@ -34,8 +32,8 @@ export default {
     this.map = new mapboxgl.Map({
       container: this.$refs.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [0, 0],
-      zoom: 2
+      center: [8.6821, 49.3988],
+      zoom: 10
     })
 
     // Create a marker for each user
@@ -54,44 +52,40 @@ export default {
       el.addEventListener('mouseenter', async () => {
         const userBlogs = this.blogs.filter(blog => blog.authorId === user.id)
 
-        // Create a new Vue instance with the Carousel component
-        const CarouselComponent = Vue.extend({
-          render(h) {
-            return h('v-carousel', {
-              props: { 
-                cycle: true, 
-                value: 0,
-                height: "400",
-                width: "200"
-              },
-              style: {
-                width: '200px', 
-                whiteSpace: 'nowrap', 
-                overflow: 'hidden', 
-                textOverflow: 'ellipsis' 
-                } 
-            }, userBlogs.map(blog => {
-                return h('v-carousel-item', { 
-                    style: { 
-                    width: '200px', 
-                    whiteSpace: 'nowrap', 
-                    overflow: 'hidden', 
-                    textOverflow: 'ellipsis' 
-                    } 
-                },
-                [
-                h('h2', {}, blog.title),
-                h('p', {}, blog.content),
-              ])
-            }))
-          },
-          vuetify: new Vuetify() 
-        })
-        const carouselInstance = new CarouselComponent()
+        let carouselHtml = `
+          <div class="swiper-container" style="width: 200px; height: 300px;">
+            <div class="swiper-wrapper">`;
 
-        // Mount the instance and extract the HTML
-        const popupHtml = carouselInstance.$mount().$el.outerHTML
-        marker.setPopup(new mapboxgl.Popup({ anchor: 'bottom' }).setHTML(popupHtml)).togglePopup()
+        userBlogs.forEach(blog => {
+          let contentSnippet = blog.content.substring(0, 200) + '...';
+
+          carouselHtml += `
+            <div class="swiper-slide" style="overflow:auto; pointer-events:none">
+              <h2>${blog.title}</h2>
+              <img src="${blog.blogImgUrl}" style="width:100%; height:150px; object-fit:cover"/>
+              <p class="blog-snippet">${contentSnippet}</p>
+            </div>`;
+        });
+
+        carouselHtml += `
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+          </div>`;
+
+        marker.setPopup(new mapboxgl.Popup({ maxWidth: '200px' }).setHTML(carouselHtml)).togglePopup()
+
+        new SwiperClass('.swiper-container', {
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+        });
       })
 
       this.markers.push(marker)
@@ -106,4 +100,34 @@ export default {
   background-size: cover;
   background-position: center;
 }
+
+/* Swiper default styles */
+.swiper-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.blog-content {
+    font-size: 10px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3; /* number of lines to show */
+    -webkit-box-orient: vertical;
+  }
+.blog-snippet {
+    font-size: small;
+}
 </style>
+
