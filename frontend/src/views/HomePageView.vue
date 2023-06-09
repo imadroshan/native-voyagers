cc5eb895f8054dae209bcc185f6b12124c8b076b
Imad Roshan <imad.roshan@gmail.com>
2023-06-10 00:20:44 +0200

    Adds a blog carousel from vuetify

---
+++ frontend/src/views/HomePageView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home-page-view">
+      <HomePage/>
+    </div>
+  </template>
+  
+  <script>
+  import HomePage from '@/components/HomePage.vue'
+  
+  export default {
+    name: 'HomePageView',
+    components: {
+      HomePage
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
